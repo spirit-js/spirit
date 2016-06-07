5d101dca60155caa1618c9ee00d63389061f0fe7
hc <hheennrryy@gmail.com>
2016-06-07 02:27:06 -0400

    add some tests

--- spec/core/response-spec.js
+++ spec/core/response-spec.js
@@ -4,11 +4,61 @@ const res = require("../support/mock-response")
 describe("core.response", () => {
 
   describe("is_response", () => {
-    it("")
+    it("returns true for valid response maps", () => {
+      const valid = [
+        { status: 123, headers: {} },
+        { status: 123, headers: {}, body: "" },
+        { status: 123, headers: {"Content-Type": "hi"} },
+      ]
+
+      valid.forEach((v) => {
+        expect(response.is_response(v)).toBe(true)
+      })
+    })
+
+    it("returns false for invalid response maps", () => {
+      const invalid = [
+        "", 123, {}, [],
+        { status: "hi", headers: {} },
+        { status: 123, body: 123 },
+        { status: 123, headers: [] }
+      ]
+
+      invalid.forEach((v) => {
+        expect(response.is_response(v)).toBe(false)
+      })
+    })
   })
 
   describe("redirect", () => {
-    it("")
+    it("generates a response map for redirecting", () => {
+      let rmap = response.redirect(123, "google")
+      expect(rmap).toEqual({
+        status: 123,
+        body: "",
+        headers: { "Location": "google" }
+      })
+
+      // defaults status to 302
+      rmap = response.redirect("blah")
+      expect(rmap).toEqual({
+        status: 302,
+        body: "",
+        headers: { "Location": "blah" }
+      })
+    })
+
+    it("throws an error for invalid arguments", () => {
+      const test = (status, url) => {
+        expect(() => {
+          response.redirect(status, url)
+        }).toThrowError(/invalid arguments/)
+      }
+
+       test(123)
+       test("blah", 123)
+       test("hi", "blah")
+    })
   })
 
   describe("json", () => {
