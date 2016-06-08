cf0eba1ba2d71cb2685293d521d094d64d824b14
hc <henycr@gmail.com>
2016-06-07 20:09:34 -0400

    add specs, refactor router.response & it's middleware
    router.response now takes a req as well
    refactor router.response middleware setup
    add specs for router.response

---
+++ spec/router/response-spec.js
@@ -0,0 +1,105 @@
+const response = require("../../lib/router/response")
+
+describe("router.response", () => {
+
+  describe("register", () => {
+
+    const orig_middlewares = response.middlewares.list()
+
+    it("adds a middleware fn to the beginning of middleware list", () => {
+      const fn = () => {}
+      response.middlewares.register(fn)
+      expect(response.middlewares.list().length).toBe(4)
+      expect(response.middlewares.list()[0]).toBe(fn)
+    })
+
+    it("will replace the middleware list with a new one when called with an array", () => {
+      // from last test
+      expect(response.middlewares.list().length).toBe(4)
+
+      response.middlewares.register(["blah"])
+      expect(response.middlewares.list().length).toBe(1)
+
+      // also resetting middlewares, important! for other tests
+      response.middlewares.register(orig_middlewares)
+      expect(response.middlewares.list().length).toBe(3)
+    })
+
+    it("throws an error if argument is not a function/array", () => {
+      expect(() => {
+        response.middlewares.register({})
+      }).toThrowError(/register expects/)
+    })
+
+    it("mutating list() does not mutate the middlewares", () => {
+      const l = response.middlewares.list()
+      const orig_len = l.length
+      l.push(123)
+      expect(response.middlewares.list().length).toBe(orig_len)
+      expect(l.length).toBe(orig_len + 1)
+    })
+  })
+
+  describe("render", () => {
+    it("calls middlewares passed in, in order until one of them returns a result", () => {
+      const rmap = {
+        blah: 1
+      }
+
+      const middlewares = [
+        (req, res) => {
+          expect(res).toEqual({
+            blah: 1
+          })
+          res.called = 1
+          // not returning goes to next middleware
+        },
+        (req, res) => {
+          expect(res).toEqual({ blah: 1, called: 1})
+          // have to render a valid response map
+          return { status: 200, headers: {}, body: "hi!" }
+        }
+      ]
+
+      const result = response.render({}, rmap, middlewares)
+      expect(result).toEqual({
+        status: 200,
+        headers: {},
+        body: "hi!"
+      })
+    })
+
+    it("throws if invalid response from response middlewares or no middleware can handle the `resp`", () => {
+      expect(() => {
+        response.render({}, "blah", [])
+        // errors since no middlewares returned a response
+      }).toThrowError(/unable to render/)
+
+      expect(() => {
+        response.render({}, "blah", [
+          () => {
+            return { status: "abc", headers: {}, body: "blah" }
+          }
+        ])
+        // errors since the middle returned an invalid map
+      }).toThrowError(/unable to render/)
+    })
+  })
+
+  describe("response", () => {
+    it("")
+  })
+
+  describe("render_string", () => {
+    it("")
+  })
+
+  describe("render_number", () => {
+    it("")
+  })
+
+  describe("render_file", () => {
+    it("")
+  })
+
+})
--- src/router/response.js
+++ src/router/response.js
@@ -1,37 +1,75 @@
+/*
+ * Higher level abstraction over core.response
+ * mostly centered around rendering response maps based on
+ * what was returned by the user
+ *
+ * Has a middleware system in place, mostly meant for developers
+ * NOTE could use the same abstraction with define/reducep/mapl
+ * for iterating over middlewares, but I don't expect response
+ * middlewares to need to handle async/Promise to generate
+ * a appropriate response map
+ *
+ */
+
 const core = require("../core/core")
 core.response = require("../core/response")
 
+// response middlewares
+const middlewares = {
+  _middlewares: [],
 
-const response_middlewares = []
+  /**
+   * register a middleware function for handling responses
+   *
+   * the order of the execution of the middlewares is
+   * last in, first out
+   *
+   * so what is registered last is called first
+   *
+   * when called with an array (of response middlewares),
+   * it will replace the entire
+   * middlewares with array as it's new middlewares
+   *
+   * @public
+   * @param {function} mw - a middleware function
+   * @return {array} list of response middlewares
+   */
+  register(mw) {
+    if (typeof mw === "function") {
+      this._middlewares.unshift(mw)
+      return this.list()
+    }
 
-/**
- * register a middleware function for handling responses
- *
- * the order of the execution of the middlewares is
- * last in, first out
- *
- * so whatever is registered last is called first
- *
- * @public
- * @param {function} mw - a middleware function
- */
-const register = (mw) => {
-  if (typeof mw !== "function") {
-    throw new TypeError("register was called with a non-function")
+    if (Array.isArray(mw)) {
+      this._middlewares = mw.slice()
+      return this.list()
+    }
+
+    throw new TypeError("register expects function/array as argument")
+  },
+
+  /**
+   * returns the array of response middlewares
+   *
+   * @return {array} response middlewares
+   */
+  list() {
+    return this._middlewares.slice()
   }
-  response_middlewares.unshift(mw)
 }
 
 /**
  * runs `resp` through response middlewares
  *
+ * @param {request-map} req - in the future a request-map, but as of right now it's a http Request object
  * @param {response-map} resp - a leaf response map
+ * @param {array} middlewares - an array of response middlewares
  * @return {response-map}
  */
-const render = (resp) => {
+const render = (req, resp, middlewares) => {
   let result
-  response_middlewares.some((fn) => {
-    result = fn(resp)
+  middlewares.some((fn) => {
+    result = fn(req, resp)
     if (typeof result !== "undefined") {
       return true
     }
@@ -51,16 +89,16 @@ const render = (resp) => {
  * @param {http.Response} res - node http Response object
  * @param {*} body - the result of a route's body function or from a middleware
  */
-const response = (res, body) => {
-  let resp = body
+const response = (req, res, body) => {
+  let rmap = body
   if (!core.response.is_response(body)) {
-    resp = core.response.response(body)
+    rmap = core.response.response(body)
   }
 
-  core.send(res, render(resp))
+  core.send(res, render(req, rmap, middlewares.list()))
 }
 
-const render_string = (resp) => {
+const render_string = (req, resp) => {
   const {status, headers, body} = resp
   if (typeof body === "string") {
     return {
@@ -73,11 +111,11 @@ const render_string = (resp) => {
   }
 }
 
-const render_number = (resp) => {
+const render_number = (req, resp) => {
   const {status, headers, body} = resp
   if (typeof body === "number") {
     resp.body = resp.body.toString()
-    return render_string(resp)
+    return render_string(req, resp)
   }
 }
 
@@ -86,16 +124,17 @@ const render_file = (resp) => {
 }
 
 // init, register default response middleware
-register(render_string)
-register(render_number)
-register(render_file)
+middlewares.register(render_string)
+middlewares.register(render_number)
+middlewares.register(render_file)
 
 module.exports = {
   response,
   render,
-  middlewares: {
-    render_file, render_string, render_number
+  renderables: {
+    file: render_file,
+    string: render_string,
+    number: render_number
   },
-  register,
-  response_middlewares
+  middlewares
 }
