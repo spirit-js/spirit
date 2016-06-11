04eca178756ae3eaad728b898b2a1f44b628290d
hc <henycr@gmail.com>
2016-06-11 01:01:40 -0400

    move redirect, stub new code

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var core = require("./lib/core/core")
-
 var defaults = require("./lib/defaults/index")
 
 var router = require("./lib/router/router")
 var routes = require("./lib/router/routes")
-
 var resources = require("./lib/router/resource")
 
+var response_map = require("./lib/router/response-map")
+
 module.exports = {
+  // core
   leaf: core.leaf,
   define: core.define,
-
   site_defaults: defaults,
 
+  // router & route
   routes: Object.assign({
     define: router.define,
     route: router.route,
+    not_found: router.not_found,
     resources: resources
-  }, routes.verbs)
+  }, routes.verbs),
+
+  // response helpers
+  response: response_map.create,
+  redirect: response_map.redirect
 }
