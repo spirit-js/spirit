754b2586b0a3d2c99cec8eb78723e01291e27521
hc <hheennrryy@gmail.com>
2016-06-14 17:18:57 -0400

    Buffers by default assume html content type
    as well as update sending_file example
    add a socket.io example

--- examples/sending_file.js
+++ examples/sending_file.js
@@ -4,43 +4,28 @@ const http = require("http")
 const Promise = require("bluebird")
 const fs = Promise.promisifyAll(require("fs"), { suffix: "Promise" });
 
-const working_path = __dirname + "/../"
-
 /*
  * Both are equivalent and show how it's possible
  * to return a file as a Promise
  */
 
 // regular Promise way
-const file = () => {
+const regular_example = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(working_path + "index.js", (err, data) => {
+    fs.readFile(__dirname + "/test-file.html", (err, data) => {
       (err) ? reject(err) : resolve(data)
     })
   })
 }
 
 // with bluebird promisify 'readFilePromise' returns a Promise already
-const promised_file = () => {
-  return fs.readFilePromise(working_path + "index.js")
-}
-
-// returning just the file may not always be desirable
-// as sometimes you may want to set certain headers so the browser
-// displays it propery
-//
-// in which case, use the response helper
-const as_response = () => {
-  // since all routes are just functions,
-  // we can re-use a existing route function to make a new route
-  const file = promised_file()
-  return response(file).type("html")
+const bluebird_example = () => {
+  return fs.readFilePromise(__dirname + "/test-file.html")
 }
 
 const app = define([
-  routes.get("/", [], file),
-  routes.get("/bluebird", [], promised_file),
-  routes.get("/response", [], as_response)
+  routes.get("/", [], regular_example),
+  routes.get("/bluebird", [], bluebird_example),
 ])
 
 const server = http.createServer(spirit([routes.route(app)]))
