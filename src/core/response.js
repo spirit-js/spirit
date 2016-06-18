6514141d513bf2bc2fb487b82851911cc3d1bf47
hc <henycr@gmail.com>
2016-06-18 18:11:13 -0400

    init core redesign

--- src/core/response.js
+++ src/core/response.js
@@ -1,29 +1,4 @@
 /**
- * writes a http response map to a node HTTP response object
- *
- * It only knows how to write string/buffer and of course stream
- *
- * NOTE: There is no guards or type checking
- *
- * @param {http.Response} res - node http response object
- * @param {response-map} resp - a leaf response map
- */
-const send = (res, resp) => {
-  // TODO handle http2
-  res.writeHead(resp.status, resp.headers)
-  if (typeof resp.body !== "undefined") {
-    if (resp.body && resp.body.pipe) {
-      resp.body.pipe(res)
-    } else {
-      res.write(resp.body)
-      res.end()
-    }
-  } else {
-    res.end()
-  }
-}
-
-/**
  * checks if `resp` is a valid response
  *
  * @param {*} resp - object to check
@@ -62,6 +37,5 @@ const internal_err = (body) => {
 module.exports = {
   internal_err,
   not_found,
-  is_response,
-  send
+  is_response
 }
